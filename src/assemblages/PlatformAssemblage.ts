2d5f4627ae8a0ae49146e0c0552f7f53ce606415
Paulo Ragonha <paulo@ragonha.me>
2020-07-11 21:08:50 +0200

    Prepare a starting platform 🍛

--- src/assemblages/PlatformAssemblage.ts
+++ src/assemblages/PlatformAssemblage.ts
@@ -4,6 +4,8 @@ import ModelComponent from '../components/ModelComponent'
 import ScaleComponent from '../components/ScaleComponent'
 import RigidBodyComponent from '../components/RigidBodyComponent'
 
+const STARTING_SIZE = 80
+
 export const createPlatform = (world: World, x: number, z: number) => {
   const stickHeight = 0.01
   const stickWidth = 0.5
@@ -13,7 +15,7 @@ export const createPlatform = (world: World, x: number, z: number) => {
   // stick
   world
     .createEntity()
-    .addComponent(PositionComponent, { x, z })
+    .addComponent(PositionComponent, { x, z: z - STARTING_SIZE })
     .addComponent(ModelComponent, { type: 'box', color: 'blue' })
     .addComponent(ScaleComponent, { y: stickHeight, x: stickWidth, z: stickWidth })
     .addComponent(RigidBodyComponent, { mass: 0, type: 'box' })
@@ -21,8 +23,17 @@ export const createPlatform = (world: World, x: number, z: number) => {
   // platform
   world
     .createEntity()
-    .addComponent(PositionComponent, { x, z, y: stickHeight })
+    .addComponent(PositionComponent, { x, z: z - STARTING_SIZE, y: stickHeight + 0.5 })
     .addComponent(ModelComponent, { type: 'box', color: 'purple' })
     .addComponent(ScaleComponent, { y: platformHeight, x: platformWidth, z: platformWidth * 3 })
     .addComponent(RigidBodyComponent, { mass: 5, type: 'box' })
+}
+
+export const createStartingPlatform = (world: World) => {
+  world
+    .createEntity()
+    .addComponent(PositionComponent, { x: 0, z: -STARTING_SIZE / 2 })
+    .addComponent(ModelComponent, { type: 'box', color: 'blue' })
+    .addComponent(ScaleComponent, { y: 0.1, x: 2, z: STARTING_SIZE })
+    .addComponent(RigidBodyComponent, { mass: 0, type: 'box' })
 }
--- src/systems/PlatformCreationSystem.ts
+++ src/systems/PlatformCreationSystem.ts
@@ -1,9 +1,9 @@
 import { System } from 'ecsy'
-import { createPlatform } from '../assemblages/PlatformAssemblage'
+import { createPlatform, createStartingPlatform } from '../assemblages/PlatformAssemblage'
 
 export default class PlatformCreationSystem extends System {
   init() {
-    createPlatform(this.world, 0, 0)
+    createStartingPlatform(this.world)
 
     for (let i = 1; i < 100; i++) {
       createPlatform(this.world, Math.random() * 6 - 3, -10 * i)
