07eed0c716892c3973c671decb9caeae9a00a0b4
Paulo Ragonha <paulo@ragonha.me>
2020-07-12 14:29:06 +0200

    Setup a simple “levels” structure 💰

--- src/assemblages/PlatformAssemblage.ts
+++ src/assemblages/PlatformAssemblage.ts
@@ -6,21 +6,79 @@ import ScaleComponent from '../components/ScaleComponent'
 import RigidBodyComponent from '../components/RigidBodyComponent'
 import PlatformTagComponent from '../tags/PlatformTagComponent'
 
+const levels = [
+  {
+    platformDepth: [18, 20],
+    platformWidth: [5, 8],
+    platformX: [-2, 2],
+    padding: [2, 4],
+    sturdiness: [0.6, 0.9],
+  },
+  {
+    platformDepth: [14, 18],
+    platformWidth: [4, 7],
+    platformX: [-2, 2],
+    padding: [2, 5],
+    sturdiness: [0.4, 0.9],
+  },
+  {
+    platformDepth: [10, 16],
+    platformWidth: [3, 7],
+    platformX: [-3, 3],
+    padding: [2, 6],
+    sturdiness: [0.3, 0.9],
+  },
+  {
+    platformDepth: [8, 14],
+    platformWidth: [2, 7],
+    platformX: [-4, 4],
+    padding: [4, 10],
+    sturdiness: [0.2, 0.9],
+  },
+  {
+    platformDepth: [5, 14],
+    platformWidth: [2, 4],
+    platformX: [-4, 4],
+    padding: [4, 12],
+    sturdiness: [0.2, 0.6],
+  },
+  {
+    platformDepth: [5, 12],
+    platformWidth: [2, 3],
+    platformX: [-5, 5],
+    padding: [15, 20],
+    sturdiness: [0.1, 0.2],
+  },
+]
+
 const STARTING_SIZE = 40
-const PLATFORM_DEPTH = 20
-const PLATFORM_WIDTH = 3
-const PADDING = 4
+const STARTING_PADDING = 20
+
+function getRandom([low, high]: number[]) {
+  const delta = high - low
+  const value = Math.random() * delta
+  return low + value
+}
+
+function getLevelFromPosition(position: number) {
+  return Math.min(Math.round((position * -1) / 100), levels.length - 1)
+}
 
 export const createPlatform = (world: World, positionZ: number) => {
-  const sturdiness = Math.random()
-  const platformColor = Color('purple').darken(sturdiness).hex()
-  const x = Math.random() * 6 - 3
+  const levelId = getLevelFromPosition(positionZ)
+  const currentLevel = levels[levelId]
+  console.log('levelId', levelId)
 
   const stickHeight = 0.01
-  const stickWidth = 2
   const platformHeight = 1
-  const platformWidth = PLATFORM_WIDTH
-  const platformDepth = PLATFORM_DEPTH
+  const sturdiness = getRandom(currentLevel.sturdiness)
+  const x = getRandom(currentLevel.platformX)
+  const platformWidth = getRandom(currentLevel.platformWidth)
+  const platformDepth = getRandom(currentLevel.platformDepth)
+  const padding = getRandom(currentLevel.padding)
+  const stickWidth = platformDepth / 10
+
+  const platformColor = Color('purple').darken(sturdiness).hex()
 
   const positionY = -0.5
 
@@ -42,7 +100,7 @@ export const createPlatform = (world: World, positionZ: number) => {
     .addComponent(RigidBodyComponent, { mass: 10 * sturdiness + 0.5, type: 'box' })
     .addComponent(PlatformTagComponent)
 
-  return positionZ - (PLATFORM_DEPTH + PADDING)
+  return positionZ - (platformDepth + padding)
 }
 
 export const createStartingPlatform = (world: World) => {
@@ -53,5 +111,5 @@ export const createStartingPlatform = (world: World) => {
     .addComponent(ScaleComponent, { y: 0.1, x: 2, z: STARTING_SIZE })
     .addComponent(RigidBodyComponent, { mass: 0, type: 'box' })
 
-  return -(STARTING_SIZE + PADDING + 10)
+  return -(STARTING_SIZE + STARTING_PADDING)
 }
